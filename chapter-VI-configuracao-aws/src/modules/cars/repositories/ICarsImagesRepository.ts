64831176207ee0ac28b0268b8e68635cf6099a1b
Wellington <wellingtonalvs@gmail.com>
2022-08-21 13:42:52 -0300

    Delete chapter-VI-configuracao-aws directory

--- chapter-VI-configuracao-aws/src/modules/cars/repositories/ICarsImagesRepository.ts
+++
@@ -1,7 +0,0 @@
-import { CarImage } from "../infra/typeorm/entities/CarImage";
-
-interface ICarsImagesRepository {
-  create(car_id: string, car_image: string): Promise<CarImage>;
-}
-
-export { ICarsImagesRepository };
--- chapter-VI-configuracao-aws/src/utils/file.ts
+++
@@ -1,5 +0,0 @@
-import fs from "fs";
-
-export const deleteFile = async (filename: string) => {
-
-}
